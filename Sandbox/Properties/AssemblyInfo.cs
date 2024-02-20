f61b7117e26374df1d7c32e6ce28b187658c18a2
Peter Patrick Eriksen <pete05q5@edu.zealand.dk>
2024-02-20 13:36:07 +0100

    updated assembly

--- Sandbox/Properties/AssemblyInfo.cs
+++ Sandbox/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sandbox")]
+[assembly: AssemblyTitle("PizzaStore")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sandbox")]
+[assembly: AssemblyProduct("PizzaStore")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
